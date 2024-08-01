480c98a8d59b5d5b366c6c74959a1b9fe85088af
Vince Chapman <vchapandrews@gmail.com>
2024-08-01 21:54:17 +0100

    fix: change to take blob input

--- app/api/transcribe/route.ts
+++ app/api/transcribe/route.ts
@@ -4,16 +4,13 @@ export const dynamic = 'force-dynamic' // defaults to auto
 
 export async function POST(req: Request) {    
     try {
-        const formData = await req.formData();
-        const file = formData.get('file');
+        const blob = await req.blob();
 
-        if (!file) {
+        if (!blob) {
             return new Response('No file uploaded', {
                 status: 400,
             });
         }
-
-        const blob = new Blob([file], { type: 'audio/wav' });
 
         const response = await query(blob);
 
