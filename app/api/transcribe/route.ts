b870d96fd441bad7987e8b6f6115830215bd74fb
Vince Chapman <95453696+vincechapman@users.noreply.github.com>
2024-08-02 13:48:28 +0100

    fix: modify route to use openai speed-to-text instead

--- app/api/transcribe/route.ts
+++ app/api/transcribe/route.ts
@@ -1,18 +1,38 @@
-import query from "./api";
+import transcribe from "../../../util/transcription/openai-whisper";
+import os from 'os';
+import fs from 'fs';
+import path from 'path';
 
-export const dynamic = 'force-dynamic' // defaults to auto
+export const dynamic = 'force-dynamic';
+
+export const config = {
+    api: {
+      bodyParser: false,
+    },
+};
 
 export async function POST(req: Request) {    
     try {
-        const blob = await req.blob();
-
-        if (!blob) {
+        const formData = await req.formData();
+        const file = await formData.get('audio') as File
+        
+        if (!file) {
             return new Response('No file uploaded', {
                 status: 400,
             });
+        } else {
+            console.log("Received file:", file);
         }
 
-        const response = await query(blob);
+        const tempdir = os.tmpdir();
+        const tempFilePath  = path.join(tempdir, file.name);
+
+        const arrayBuffer = await file.arrayBuffer();
+        fs.writeFileSync(tempFilePath, Buffer.from(arrayBuffer));
+
+        const response = await transcribe(tempFilePath);
+
+        console.log("Transcription complete:", response);
 
         return new Response(JSON.stringify(response), {
             headers: {
@@ -21,6 +41,7 @@ export async function POST(req: Request) {
         });
     
     } catch (error) {
+        console.error("Something went wrong in /api/transcribe route:", error);
         return new Response('Error uploading file: ' + error, {
             status: 500,
         });
