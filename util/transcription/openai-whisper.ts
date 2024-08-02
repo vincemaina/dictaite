03987275993b62244097f06350b9a4baef9582eb
Vince Chapman <95453696+vincechapman@users.noreply.github.com>
2024-08-02 14:03:03 +0100

    refactor: add types for function output

--- util/transcription/openai-whisper.ts
+++ util/transcription/openai-whisper.ts
@@ -5,10 +5,31 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+interface OpenAITranscriptionSegment {
+    "id": number,
+    "seek": number,
+    "start": number,
+    "end": number,
+    "text": string,
+    "tokens": number[],
+    "temperature": number,
+    "avg_logprob": number,
+    "compression_ratio": number,
+    "no_speech_prob": number
+}
+
+export interface OpenAITranscriptionObject {
+    "task": string,
+    "language": string,
+    "duration": number,
+    "text": string,
+    "segments": OpenAITranscriptionSegment[]
+}
+  
 /**
  * Transcribes an audio file using the OpenAI Whisper API
  */
-export default async function transcribe(filePath: string) {
+export default async function transcribe(filePath: string): Promise<OpenAITranscriptionObject> {
 
     if (!filePath.trim()) {
         throw new Error("No file path provided");
@@ -20,7 +41,7 @@ export default async function transcribe(filePath: string) {
         response_format: "verbose_json",
         timestamp_granularities: ["segment"]
         // TODO: look into adding language param https://platform.openai.com/docs/api-reference/audio/createTranscription#audio-createtranscription-language
-    });
+    }) as OpenAITranscriptionObject;
 
     console.log(transcription);
 
