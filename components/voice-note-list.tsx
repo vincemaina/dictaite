b9c7fe04606bd06108e84172eadc04b1cdd50495
Vince Chapman <95453696+vincechapman@users.noreply.github.com>
2024-08-03 17:47:49 +0100

    feat: add a search bar

--- components/voice-note-list.tsx
+++ components/voice-note-list.tsx
@@ -1,26 +1,40 @@
+import SearchIcon from "./icons/search";
 import VoiceNote from "./voice-note";
 
 export default function VoiceNoteList() {
     return (
-        <div className="h-screen w-96 sticky top-0 bg-neutral-900 p-4 flex flex-col gap-3">
-            <VoiceNote
-                title="Testing Voice Transcription with Pookie After Recent Updates"
-                date={new Date()}
-                seconds={120}
-                tags={["tag1", "tag2"]}
-            />
-            <VoiceNote
-                title="Testing Voice Transcription with Pookie After Recent Updates"
-                date={new Date()}
-                seconds={10}
-                tags={["tag1", "tag2"]}
-            />
-            <VoiceNote
-                title="Testing Voice Transcription with Pookie After Recent Updates"
-                date={new Date()}
-                seconds={10}
-                tags={["tag1", "tag2"]}
-            />
+        <div className="h-screen w-96 sticky top-0 bg-neutral-900 p-6">
+
+            <div className="fill-gray-300 flex gap-3 items-center mb-5 px-1">
+                <SearchIcon/>
+                <input type="search"
+                    name="input-search"
+                    id="input-search"
+                    className="bg-transparent w-full placeholder:text-sm"
+                    placeholder="Search recordings"
+                />
+            </div>
+
+            <div className="flex flex-col gap-3">
+                <VoiceNote
+                    title="Testing Voice Transcription with Pookie After Recent Updates"
+                    date={new Date()}
+                    seconds={120}
+                    tags={["tag1", "tag2"]}
+                />
+                <VoiceNote
+                    title="Testing Voice Transcription with Pookie After Recent Updates"
+                    date={new Date()}
+                    seconds={10}
+                    tags={["tag1", "tag2"]}
+                />
+                <VoiceNote
+                    title="Testing Voice Transcription with Pookie After Recent Updates"
+                    date={new Date()}
+                    seconds={10}
+                    tags={["tag1", "tag2"]}
+                />
+            </div>
         </div>
     )
 }
